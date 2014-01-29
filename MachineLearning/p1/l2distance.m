f7b669ea77865a04cbf02659025406bd99ee17c6
guobingkun <guobingkun@hotmail.com>
2014-01-29 02:01:00 -0600

    add

---
+++ MachineLearning/p1/l2distance.m
@@ -0,0 +1,29 @@
+function D=l2distance(x1,x2)
+% function D=l2distance(x1,x2)
+%	
+% Computes the Euclidean distance matrix. 
+% Syntax:
+% D=l2distance(x1,x2)
+% Input:
+% x1: dxn data matrix with n vectors (columns) of dimensionality d
+% x2: dxm data matrix with m vectors (columns) of dimensionality d
+%
+% Output:
+% Matrix D of size nxm 
+% D(i,j) is the Euclidean distance of x1(:,i) and x2(:,j)
+%
+% call with only one input:
+% l2distance(x1)=l2distance(x1,x1)
+%
+
+if (nargin==1) % case when there is only one input (x1)
+	%% fill in code here
+
+else  % case when there are two inputs (x1,x2)
+	%% fill in code here
+
+end;
+%
+
+
+
